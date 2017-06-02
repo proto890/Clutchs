576b481206e67ad99339352561d71f1bc0a4d88e
proto890 <proto890@googlemail.com>
2017-06-02 18:57:02 +0100

    Movement

---
+++ Assets/Code/Movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+public float speed;
+
+private Rigidbody player;
+
+
+
+	// Use this for initialization
+	void Start () {
+					player = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+					transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+
+					float moveHori = Input.GetAxis ("Horizontal");
+					
+					float moveVeri = Input.GetAxis ("Vertical");
+
+					Vector3 movement = new Vector3 (moveHori, 0.0f, moveVeri);
+
+        			player.AddForce (movement * speed);
+
+
+	}
+}
