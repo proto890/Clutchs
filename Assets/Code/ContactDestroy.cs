692bd489a4935c3a0979dd1050afd553a8fd433d
proto890 <proto890@googlemail.com>
2017-06-07 21:00:58 +0100

    Prototype update
    Now we’re cooking

---
+++ Assets/Code/ContactDestroy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContactDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider other) 
+	{
+
+
+		if (other.tag == "Road")
+		{
+			return;
+			//Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			//gameController.GameOver ();
+		}
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Code/HazardMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HazardMover : MonoBehaviour {
+
+	public float speed;
+
+
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
