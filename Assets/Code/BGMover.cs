0b247bb532c7ca4ba24cfc578ce023caf01d2e52
proto890 <proto890@googlemail.com>
2017-06-13 20:37:51 +0100

    More stuff

---
+++ Assets/Code/BGMover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMover : MonoBehaviour {
+
+	public float scrollSpeed;
+	public float tileSizeZ;
+
+	private Vector3 start;
+
+
+
+	// Use this for initialization
+	void Start () {
+		start = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ);
+		transform.position = start + Vector3.forward * newPosition;
+	}
+}
